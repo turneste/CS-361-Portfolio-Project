247f9d468ce59cdb8a0ff1a0d63c3774786c771d
Steven Turner <71907094+turneste@users.noreply.github.com>
2021-10-18 18:49:36 -0400

    Started app

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class Travel_Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    age = db.Column(db.Integer, nullable=False)
+    city_depart = db.Column(db.String(200), nullable=False)
+    budget = db.Column(db.Integer, nullable=False)
+    interest = db.Column(db.Integer, nullable=False)  # list
 
 @app.route('/')
 def index():
-    return "Hello World"
+    data = Travel_Data.query.all()
+    return render_template('index.html', data=data)
 
+#  db.create_all()
+#  intialize()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
